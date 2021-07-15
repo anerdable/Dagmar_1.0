606c194bad2974f443bba5bd09ae608903d36dbd
Paula D'Cruz <paula.dcruz@r2m.se>
2021-07-15 15:35:08 +0200

    add foundation for slackbot

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,20 @@
 export * from './lib/async';
 export * from './lib/number';
+
+const path = require('path')
+ require('dotenv').config({ path: path.resolve(__dirname, '../.env') })
+
+ console.log("dagmar äger");
+
+// Require the Bolt package (github.com/slackapi/bolt)
+const { App } = require("@slack/bolt");
+const app = new App({
+    token: 'xoxb-2262518295655-2262705096423-Hj5dLKN4gFDOgRVDOUpTo3WN',
+    signingSecret: 'eaf51c216db5eb883a6de821e2ae0840'
+  });
+  // All the room in the world for your code
+  (async () => {
+    
+  // Start your app
+    await app.start(process.env.PORT || 3000);  console.log('⚡️ Bolt app is running!');
+  })();
